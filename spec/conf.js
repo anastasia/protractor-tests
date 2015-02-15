983cfabb06ba8c9abf3b01bed3eeaf93bce9655e
Anastasia Aizman <anastasia.aizman@gmail.com>
2015-02-15 12:13:01 -0800

    update - change baseUrl, remove seleniumAddress

--- spec/conf.js
+++ spec/conf.js
@@ -9,17 +9,17 @@ var browsers = {
   }
 }
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  browsers: {
+    chrome: {
+      name: 'chrome',
+      browserName: 'chrome'
+    }
+  },
   specs: ['./tests/*.coffee'],
-  baseUrl: "http://localhost:8000",
+  baseUrl: "http://localhost:5555",
   rootElement: '.my-app',
   jasmineNodeOpts: {
     showColors: true
   }
 }
 
-if (process.argv[3] == '--chrome') {
-  exports.config.capabilities = browsers.chrome
-} else {
-  exports.config.multiCapabilities = [browsers.firefox, browsers.chrome]
-}
