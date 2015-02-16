e7e65f7c82e7ea9511b1e31310511fe109336bd5
Anastasia Aizman <anastasia.aizman@gmail.com>
2015-02-15 21:25:46 -0800

    update - adding browser chrome

--- spec/conf.js
+++ spec/conf.js
@@ -1,5 +1,8 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
+  capabilities: {
+    'browserName': 'chrome'
+  },
   keepAlive: true,
   allScriptsTimeout: 11000,
   specs: ['./tests/*.coffee'],
