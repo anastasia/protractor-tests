5ac65cdc59b3a7e158ff22b18cce6f2178ccebb2
Anastasia Aizman <anastasia.aizman@gmail.com>
2015-02-16 18:15:24 -0800

    add - on prepare block, checking current url

--- spec/conf.js
+++ spec/conf.js
@@ -18,6 +18,16 @@ exports.config = {
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000
+  },
+  onPrepare: function() {
+    browser.driver.get('http://localhost:3300')
+    browser.driver.findElement(By.id('importantbutton'))
+    browser.driver.wait(function(){
+      return browser.driver.getCurrentUrl().then(function(url){
+        console.log('currentUrl:', url)
+        return url
+      });
+    });
   }
 
 }
